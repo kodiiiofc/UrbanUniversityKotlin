979de631dc4042477853f931065e8eb1664d8941
Roman Mokhnatkin <kodiiiofc@gmail.com>
2024-07-05 10:08:51 +0500

    Домашнее задание по теме "Переменные и типы данных"

---
+++ src/variables/Main.kt
@@ -0,0 +1,10 @@
+package variables
+
+fun main() {
+    val age : Int = 27
+    val name : String = "Roman Mokhnatkin"
+    val isStudent : Boolean = true
+    val salary : Double = 1700.00
+    println("Age: " + age + " | Name: " + name + " | Is student: " + isStudent + " | Salary: " + salary)
+
+}
