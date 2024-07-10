f1528dc3759d1015c819049fcf2f603c4c1b35f9
Roman Mokhnatkin <kodiiiofc@gmail.com>
2024-07-10 16:37:22 +0500

    Домашнее задание по теме "Оператор For"

--- src/operatorfor/task2.kt
+++ src/operatorfor/task2.kt
@@ -1,9 +1,19 @@
 package operatorfor
 
+const val PI = 3.14
 
 fun main() {
-    val circleS : Double = 3144.0
-    val circleD = Math.sqrt((4 * circleS / Math.PI))
-    val circleL = Math.PI * circleD
+    val circleS = 3144.0
+    var circleD = 0.0
+
+    for (i in 0 .. (circleS.toInt() * 100)) {
+        if (i * i * PI / 4 >= circleS * 100) {
+            circleD = i / 10.0
+            break
+        }
+    }
+
+
+    val circleL = PI * circleD
     println("Диаметр окружности диаметром $circleS равен $circleD. Длина такой окружности $circleL")
 }
